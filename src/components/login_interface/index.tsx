1e2b4f0dbbe030a6664185fe290995e40ed8bd2a
Pedro Henrique Feitosa <50965091+pedrofeitosa97@users.noreply.github.com>
2023-05-12 17:33:21 -0300

    Feat: Criado corpo da aplicação em front

---
+++ src/components/login_interface/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Paper } from '@material-ui/core';
+
+export default function LoginInterface() {
+    return (
+        <Paper style={{ backgroundColor: 'black' }}>
+          Teste
+        </Paper>
+    );
+}
